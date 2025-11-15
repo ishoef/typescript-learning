d51b6e84706cdf09ecc594557d686e941148c78c
ishoef <ismailhossennayeb@gmail.com>
2025-11-15 20:32:58 +0200

    added destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -1,3 +1,31 @@
-// object destructureing
+// object destructuring
 // array destructuring
 
+// object destructuring
+const user = {
+  id: 101,
+  name: {
+    firstName: "Ismail",
+    middleName: "Hossan",
+    lastName: "Nayef",
+  },
+  gender: "male",
+  age: 25,
+  isLoggedIn: true,
+};
+
+const {
+  name: { firstName, lastName, middleName },
+  id,
+  gender: sex, // aliasing
+  age,
+  isLoggedIn,
+} = user;
+
+console.log(`the ${id} is for Mr. ${firstName}`);
+
+// array destructuring
+const numbers = [23, 45, 67, 89, 12, 34, 56];
+
+const [first, second, third, ...rest] = numbers;
+console.log(rest, first);
