62bb17541993cd7bca3dff5b7a8a4e14270130e0
ishoef <ismailhossennayeb@gmail.com>
2025-11-18 00:01:20 +0200

    new type

--- src/genericWtihFunction.ts
+++ src/genericWtihFunction.ts
@@ -19,3 +19,45 @@ const arrNumbers = createArrayWithGeneric(222);
 const arrObjs = createArrayWithGeneric({ id: 123, name: "ismail" });
 
 console.log(arrStrings, arrNumbers, arrObjs);
+
+// Tuple
+const createArrayWithTuple = (param1: string, param2: string) => [
+  param1,
+  param2,
+];
+
+const createArrayTupleWithGeneric = <X, Y>(param1: X, param2: Y) => [
+  param1,
+  param2,
+];
+
+const res1 = createArrayTupleWithGeneric("Ismail", 25);
+const res2 = createArrayTupleWithGeneric("Nayef", 25);
+const res3 = createArrayTupleWithGeneric("Nayef", "Student");
+
+// Function with Return Value Object
+
+const addStudentToCourse = <T>(studentInfo: T) => {
+  return {
+    courseName: "Advanced Arabic",
+    ...studentInfo,
+  };
+};
+
+const student1 = {
+  id: 123,
+  name: "Nayef",
+  hasPen: true,
+};
+
+const student2 = {
+  id: 321,
+  name: "ismail",
+  hasCar: false,
+  isMarried: true,
+};
+
+const result = addStudentToCourse(student1);
+const result2 = addStudentToCourse(student2);
+
+console.log(result, result2);
